24effa240245bcdafd35d9be7de5d1d3e4a09481
IshaanPuri <IshaanPuri@HSSLPT-677>
2021-04-14 01:25:28 +0530

    second commit

--- WebApplication1/obj/Debug/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("6f118325-38ba-4cc7-b2da-7751aa9605e7")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
